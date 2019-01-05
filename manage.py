589369ef5433a7e57fb316a832bdb2f885bb9b56
tangzhenming <2234224472@qq.com>
2019-01-05 21:10:26 +0800

    创建管理员

--- manage.py
+++ manage.py
@@ -5,31 +5,46 @@
 @Email:2234224472@qq.com
 @File:manage.py
 """
-
 from flask_script import Manager
-from flask_migrate import Migrate,MigrateCommand
-from info import create_app,db,models
+from flask_migrate import Migrate, MigrateCommand
 
-
-
+from info import create_app, db, models
 
 # 通过指定的配置名字创建对应配置的app
 # creat_app就类似与工厂方法
+from info.models import User
+
 app = create_app("development")
 
 manager = Manager(app)
 
 # 将app与db关联
-Migrate(app,db)
+Migrate(app, db)
 
 # 将迁移命令添加到manager中
-manager.add_command('db',MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
 
+@manager.option('-n','-name',dest="name")
+@manager.option('-p','-password',dest="password")
+def createsuperuser(name, password):
 
+    if not all([name, password]):
+        print("参数不足")
 
+    user = User()
+    user.nick_name = name
+    user.mobile = name
+    user.password = password
+    user.is_admin = True
 
-
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+    print("添加成功")
 
 
 if __name__ == '__main__':
